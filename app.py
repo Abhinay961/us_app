bd97a498d1e684a29cca02a9d9e80fe3c7ed5b0e
Abhinay961 <abhinay961@gmail.com>
2026-04-18 20:33:25 +0530

    fixed socketio runtime issue

--- app.py
+++ app.py
@@ -730,4 +730,4 @@ def download_export():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    socketio.run(app, debug=True, port=5000)
+    app.run(debug=True, port=5000)
